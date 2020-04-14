fc239bc048520397aafd4f50e0a6344e76e9be3b
Hasnat Hasib <30808011+HHasib@users.noreply.github.com>
2020-04-13 23:49:46 -0400

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,19 @@
+import turtle
+bob =turtle.Turtle()
+bob.shape("turtle")
+bob.shapesize(1)
+bob.pensize(8)
+bob.speed(1)
+
+
+bob.pd()
+bob.left(90)
+bob.forward(200)
+bob.forward(-100)
+bob.right(90)
+bob.forward(130)
+bob.left(90)
+bob.backward(100)
+bob.forward(200)
+
+
