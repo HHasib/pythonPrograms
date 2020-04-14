fff0d109f46e87044cc61e05e0f1983cc3c4d6db
Hasnat Hasib <30808011+HHasib@users.noreply.github.com>
2020-04-13 23:42:50 -0400

    without unnecessary comments

--- hourglass.py
+++ hourglass.py
@@ -1,4 +1,5 @@
-#ned to use nested for loops
+
+#need to use nested for loops
 
 # 
 # |""""""""""|
@@ -27,33 +28,7 @@ def hourglass():
 		for n in range(4,0,-1):
 			print(" "*n,"/",":"*(8-2*n),"\\")	
 			
-# def hourglass():
-# 	print("|","\""*10,"|")
-# 	
-# 	for i in range(4):
-# 		print(" "*i,"\\",":"*(8-2*i),"/")	
-# 		
-# 	print("      ||")
-# 	
-# 	for i in range(4):
-# 		print(" "*(3-i),"/",":"*2*(i+1),"\\")
-# 		
-# 	print("|","\""*10,"|")
-# 	
-	
-# def hourglass():
-# 	
-# 	for i in range(2):
-# 		print("|","\""*10,"|")
-# 		
-# 		for i in range(4):
-# 			print(" "*i,"\\",":"*(8-2*i),"/")	
-# 		
-# 		print("      ||")
-# 	
-# 		for i in range(4):
-# 			print(" "*(3-i),"/",":"*2*(i+1),"\\")
-	
+
 
 hourglass()
 
